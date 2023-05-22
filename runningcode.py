11eec7a1296f0655bbdc5376658a6472fbec9131
jreid13 <131926580+jreid13@users.noreply.github.com>
2023-05-22 12:12:06 -0700

    Update runningcode.py

--- runningcode.py
+++ runningcode.py
@@ -124,7 +124,7 @@ class piRobot():
         time.sleep(0.01)
         self.setFIODrive(4, 0)
         time.sleep(0.01)
-
+"""
   def ReverseTurn90(self, Direction= "Right"):
       if Direction == "Right":
           self.TurnMotor(65, "Left")
@@ -161,12 +161,6 @@ class piRobot():
           self.DriveMotor(360, "Forward")
           self.TurnMotor(65, "Right")
 
-  def reset(self):
-      self.UhOh()
-      if self.angle >0:
-          self.TurnMotor(Steering_Angle.angle, "Left")
-      if self.angle <0:
-          self.TurnMotor(abs(Steering_Angle.angle), "Right")
 
   def DriveStraight(self, cm = 1, Direction = "Forward"):
       if self.angle >0:
@@ -174,7 +168,16 @@ class piRobot():
       if self.angle <0:
           self.TurnMotor(abs(Steering_Angle.angle), "Right")
       selt.DriveMotorCM(cm, Direction)
-   
+      """
+
+
+  def reset(self):
+       self.UhOh()
+       if self.angle >0:
+           self.TurnMotor(Steering_Angle.angle, "Left")
+       if self.angle <0:
+           self.TurnMotor(abs(Steering_Angle.angle), "Right")
+
   def irMotor(self, angle = 0, Direction = "Left"):
     stepAngle = 360/4096*8;
     if Direction == "Left":
@@ -241,23 +244,149 @@ class piRobot():
       return ainValue
  
 
-       
+  def VoltagetoDistance(self, n=0):
+      Voltage = self.getAIN(n)
+      if Voltage > 0.46:
+          x = np.log((Voltage-0.46)/3.67)/(-0.068)
+          return x
+      else:
+          x = 100
+          return x      
 
-  def sensor_loop():
-      while True:
-          sensor_data = VoltagetoDistance()
-          robotloc.update_sight(sensor_data)
-          if sensor_data >= 36:
-              event_queue.put("forward")
-              time.sleep(1.25)
-              print(sensor_data)
-              print("Forward")
-          elif sensor_data < 36:
-              event_queue.put("turn")
-              time.sleep(1.25)
-              print(sensor_data)
-              print("Turn")
+  def TurntoAngle(angle): #This code turns the scope to a specific angle relative to the car
+      current_angle = self.irangle
+      final_dest = angle - current_angle
+      if final_dest > 0:
+          TurnScope(abs(final_dest), "Right")
+          return
+      else:
+          TurnScope(abs(final_dest), "Left")
+          return
 
+
+ def ScopeScan(): #This code will take an array of data in a 20 degree range. I think it can be imporved by using TurntoAngle(), as this array isnt great in terms of where the scope is pointing as there is no perfect middle measurement
+    angle_increment = 20
+    num_steps = 180//angle_increment
+    ir_data = np.zeros(num_steps)
+    self.TurnScope(90, "Left")
+    self.update_irangle(self.irangle-90)
+    for i in range(num_steps):
+        time.sleep(0.1)
+        ir_data[i] = self.VoltagetoDistance(1)
+        self.iupdate_irangle(self.irangle + angle_increment)
+        self.TurnScope(angle_increment, "Right")
+    self.TurnScope(90, "Left")
+    self.update_irangle(self.irangle-90)
+    min_value = np.min(ir_data)
+    min_index = np.argmin(ir_data)
+    print(ir_data, min_value, min_index)
+    return ir_data
+   
+   
+ stop_flag = False 
+
+ def sensor_loop(self): #This is the main sensing decision making code
+    front_min = 45 # cm Minimum values telling the car when to stop
+    diag_min = 45 #cm Minimum values for the diagonal turns. These are the two sensors pointing off to the sides
+    global stop_flag
+    while not stop_flag: #do this loop when the stop flag is false
+        front_dist = self.VoltagetoDistance(0) #scan the front 
+        if front_dist >= front_min: #if we still have space drive forward
+            print("Continuing straight...") #for me to know what the robot is thinking
+            event_queue.put("Drive") #passes drive to the queue loop, see the queue loop for more detail
+            time.sleep(0.1) #This sleeps the code to give the motor time to do its thing. If it doesnt sleep it will make a massive queue of drives, so when the stop command comes its so late that the robot will just crash lol
+        else: #front sensor isnt happy anymore. Passes to next layer of decision making.
+            #diag_right_dist = VoltagetoDistance(2) #These would be the AIN readings from the left and right sensons once mounted, respectively
+            #diag_left_dist = VoltagetoDistance(3)
+            diag_left_dist = 10 #I have these set arbitrarily to always trigger the next loop where the scanner decides
+            diag_right_dist = 10
+            print("Stopping the robot...")
+            event_queue.put("Stop") 
+            
+            if diag_min <= diag_right_dist and diag_min <= diag_left_dist: #If both left and right look okay, arbitrarily go right
+                print("Turning right...")
+                event_queue.put("Right Turn")
+                break
+            elif diag_min <= diag_right_dist: #If right is good, go right
+                print("Turning right...")
+                event_queue.put("Right Turn")
+                break
+            elif diag_min <= diag_left_dist: #If left is good, go left
+                print("Turning left...")
+                event_queue.put("Left Turn")
+                break
+            else: #If neither is good, go onto the scanning loop
+                array_data = self.ScopeScan() 
+                decision_array = [array_data[4],array_data[3],array_data[5],array_data[2],array_data[6],array_data[1],array_data[7],array_data[0],array_data[8]] #This will take the IR data and list it in a way that goes center, then direcetly to the right of center, directly to the left, second from the right and so on. This allows the robot to pick the most optimal path
+                for i, dist in enumerate(decision_array):
+                    if front_min <= dist:
+                        if i % 2 == 0: #This modulo is essentially saying right or left. All odd arrays correspond to right turns, all even correspond to left turns
+                            if i == 0: #This is because the scanner is behind the front IR sensor. Sometimes, the scanner will read an ok value in front, when in reality there isnt. Therefore, the code just ignores it.
+                                continue 
+                            else:
+                                print(f"Turning to angle {(((i)/2)*(-1)**i) * 20 } degrees...") #This tells the angle of the turn, measured from centerline. Negative values correspond to left turns
+                                angle = (((i)/2)*(-1)**i) * 20
+                                event_queue.put("Angle Turn", angle) #I want to pass both a queue event and a variable to an event queue, but it doesnt work just yet.
+                                stop_flag = True #This will stop the scanning queue, allowing a different loop to take over
+                                break
+                        else: #same as above but for the other direction
+                            print(f"Turning to angle {(((i+1)/2)*(-1)**i) * 20 } degrees...")
+                            angle = (((i)/2)*(-1)**i) * 20
+                            event_queue.put("Angle Turn", angle)
+                            stop_flag = True
+                            break
+                    elif i+1 == len(decision_array): #I havent coded this yet, but this is the option for going backwards cause the robot messed up
+                        print("didn't find acceptable range before middle of array, turn around")
+                        break
+
+                        
+  def avoid_loop(self, angle):
+    i = 0 #This i value is used to make sure that once the car is turned 90 degrees itll stop turning and just go straight
+    while True:
+       if angle > 0: #These just turn the car in the right direction 
+           self.TurnMotor(65, "Right")
+       elif angle < 0:
+           self.TurnMotor(65, "Left")
+       while i < 500: #So while the car isnt turned 90 yet
+           print("Starting to Turn")
+           front_dist = self.VoltagetoDistance(0)
+           if 10 < front_dist < 100: #Drive forward, ie turn
+               self.DriveMotor(1, "Forward")
+               i += 1
+               #selfavoidlogic.updateanglesteps() #This is keeping track of the amount of steps that the car has turned at an angle, used to retrace later
+           if front_dist < 10: #If in this process the robot gets to close to an obstace the robot gets sad. We can code something here to have it reverse or something
+               print("ima kms")
+               break
+           if front_dist >= 100: #Once the car has turned far enough that the front is clear, we now will drive until we are fully past the obstace. NOTHING FROM HERE ON HAS BEEN TESTED AND FUNCITONAL
+               print("Checking the Hip")
+               if angle > 0:
+                   self.TurnScope(abs(angle), "Left") #This turns the scope to look at the obstace, and measure if the side is clear. 
+               elif angle < 0:
+                   self.TurnScope(abs(angle), "Right")
+               side_dist = VoltagetoDistance(1)
+               if abs(scopeangle.angle) > 0: 
+                   side_dist = self.VoltagetoDistance(1)
+                   if side_dist < 100: #If the side isnt clear, keep turning the car
+                       self.DriveMotor(1, "Forward")
+                       i += 1
+                   if side_dist >= 100: #Now that the scanner cant see anything, it passes the return to course loop. I have no clue if this will even work
+                       print("returning to course")
+                       event_queue.put("RtC")
+       while i == 500:  #Once the car has turned 90 degress
+           print("made it to 90")
+           if Steering_Angle.angle > 0: #This two commands will turn the steering straight, then will turn the scope to look directly to the side where the obstacle would be
+               self.TurnMotor(65, "Left")
+               self.TurntoAngle(-90)
+           if Steering_Angle.angle < 0:
+               self.TurnMotor(65, "Right")
+               self.TurntoAngle(65)
+           side_dist = self.VoltagetoDistance1()   
+           if side_dist < 100: #Drive until the scope doesnt see anything on the side
+               self.DriveMotor(1, "Forward")
+               #avoidlogic.updatestraightsteps() #Keep track of the amount of steps taken directly straight, used for return to course
+           if side_dist >= 100:
+               event_queue.put("RtC") #Side is clear, go back to the og course
+               print("returning to course")                     
 
   def event_loop():
       while True:
@@ -272,19 +401,6 @@ class piRobot():
   event_thread = threading.Thread(target=event_loop)
 
 
-  def VoltagetoDistance(self, n):
-      Voltage = self.getAIN(n)
-      x = np.log((Voltage-0.46)/3.67)/(-0.068)
-      return x
-  
-  def VoltageWorked(self):
-      x = self.VoltagetoDistance(1)
-      if x >= 0:
-         self.irMotor(90, "Right")
-         return
-      else:
-         return
-
   def GOGOGO():
       sensor_thread.start()
       event_thread.start()
@@ -297,5 +413,5 @@ class piRobot():
 #event_queue = queue.Queue()
 
 myRobot = piRobot()
-myRobot.DriveMotor(100, "Forward")
+myRobot.irMotor(80, "Left")
 
